ff9c7d3d87c0323fdcb91a333f049ca8db82b498
lbarbele <luan.arbeletche@gmail.com>
2023-03-10 11:26:23 -0300

    by default, get a sorted file list

--- src/util/get_file_list.py
+++ src/util/get_file_list.py
@@ -1,14 +1,20 @@
 import glob
 
 def get_file_list(
-  globs: str | list[str] | tuple[str]
+  globs: str | list[str] | tuple[str],
+  sort: bool = True,
 ) -> list[str]:
   if isinstance(globs, str):
     fs = glob.glob(globs)
     if len(fs) == 0:
       raise RuntimeError(f'bad path "{globs}"')
+    if sort:
+      fs.sort()
     return fs
   elif isinstance(globs, list) or isinstance(globs, tuple):
-    return [f for g in globs for f in get_file_list(g)]
+    fs = [f for g in globs for f in get_file_list(g, sort = False)]
+    if sort:
+      fs.sort()
+    return fs
   else:
     raise RuntimeError(f'get_file_list: bad input "{globs}"')
