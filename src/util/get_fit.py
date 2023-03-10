e6d96ae3c62c7ed95b26a33e5dde995eb9fd6dab
lbarbele <luan.arbeletche@gmail.com>
2023-03-10 15:28:52 -0300

    accept sigma and return chi2

--- src/util/get_fit.py
+++ src/util/get_fit.py
@@ -1,12 +1,14 @@
 import numpy as np
 import scipy.optimize as opt
 
-def get_fit(f, x, y):
+def get_fit(f, x, y, s = None):
   # perform the fit
-  popt, pcov, info, mesg, ierr = opt.curve_fit(f, x, y, p0 = f.guess(x, y), full_output = True)
+  popt, pcov, info, mesg, ierr = opt.curve_fit(f, x, y, p0 = f.guess(x, y), full_output = True, sigma = s)
   # compute parameter errors from the cov. matrix
   perr = np.sqrt(pcov.diagonal())
   # status flag
   status = 1 <= ierr and ierr <= 4
+  # compute chi2
+  chi2 = ((y - f(x, *popt))**2).sum() if s is None else (((y - f(x, *popt))/s)**2).sum()
   # return parameters, errors, and status
-  return popt, perr, status
+  return popt, perr, status, chi2
